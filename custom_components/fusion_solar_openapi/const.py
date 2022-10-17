85d0dc255b7bdc88fb7b1dd82d954c593ac92fdf
Olivier Bossaer <olivier.bossaer@gmail.com>
2022-10-17 20:36:14 +0200

    Sensors:  + State  + Realtime Power
    Api:
     Use new endpoints:
     - getDevList
     - getDevRealKpi
    
    Refacto:
    - DEVICE_CLASS_ENERGY to SensorDeviceClass.ENERGY
    - API use Session now

--- custom_components/fusion_solar_openapi/const.py
+++ custom_components/fusion_solar_openapi/const.py
@@ -12,18 +12,55 @@ ATTR_FAIL_CODE = 'failCode'
 ATTR_SUCCESS = 'success'
 ATTR_DATA_REALKPI = 'dataItemMap'
 ATTR_DATA_STATIONCODE = 'stationCode'
+ATTR_DATA_DEVID = 'devId'
+ATTR_DATA_DEVTYPEID = 'devTypeId'
 
 # Data attributes
 ATTR_TOTAL_CURRENT_DAY_ENERGY = 'day_power'
 ATTR_TOTAL_CURRENT_MONTH_ENERGY = 'month_power'
 ATTR_TOTAL_LIFETIME_ENERGY = 'total_power'
+ATTR_INVERTER_STATE = 'inverter_state';
+ATTR_ACTIVE_POWER = 'active_power';
 
 # Possible ID suffixes
+ID_REALTIME_POWER = 'realtime_power'
+ID_STATE = 'state'
 ID_TOTAL_CURRENT_DAY_ENERGY = 'total_current_day_energy'
 ID_TOTAL_CURRENT_MONTH_ENERGY = 'total_current_month_energy'
 ID_TOTAL_LIFETIME_ENERGY = 'total_lifetime_energy'
 
 # Possible Name suffixes
+NAME_REALTIME_POWER = 'Realtime Power'
+NAME_STATE = 'State'
 NAME_TOTAL_CURRENT_DAY_ENERGY = 'Total Current Day Energy'
 NAME_TOTAL_CURRENT_MONTH_ENERGY = 'Total Current Month Energy'
 NAME_TOTAL_LIFETIME_ENERGY = 'Total Lifetime Energy'
+
+InverterState = {
+    0: 'Standby: initializing',
+    1: 'Standby: insulation resistance detection',
+    2: 'Standby: sunlight detection',
+    3: 'Standby: power grid detection',
+    256: 'Start',
+    512: 'Grid connection',
+    513: 'Grid connection: limited power',
+    514: 'Grid connection: self-derating',
+    768: 'Shutdown: unexpected shutdown',
+    769: 'Shutdown: commanded shutdown',
+    770: 'Shutdown: OVGR',
+    771: 'Shutdown: communication disconnection',
+    772: 'Shutdown: limited power',
+    773: 'Shutdown: manual startup is required',
+    774: 'Shutdown: DC switch disconnected',
+    1025: 'Grid scheduling: cosψ-P curve',
+    1026: 'Grid scheduling: Q-U curve',
+    1280: 'Spot-check ready',
+    1281: 'Spot-checking',
+    1536: 'Inspecting',
+    1792: 'AFCI self-check',
+    2048: 'I-V scanning',
+    2304: 'DC input detection',
+    40960: 'Standby: no sunlight',
+    45056: 'Communication disconnection (written by the SmartLogger)',
+    49152: 'Loading (written by the SmartLogger)'
+}
